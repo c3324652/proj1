6d7dd3b327d37873335b132ae237385ac37cde50
matthew ptolemy <c3324652@uon.edu.au>
2019-04-11 06:51:06 +0000

    week 7 lab 2

--- task1.c
+++ task1.c
@@ -7,18 +7,21 @@
 
 int main()
 {
+    int i;
     FILE *input;
     input=fopen("data.txt","r");
         if(input == NULL){
             perror("fopen()");
             return 0;
 }
-
-    char str[20];
+    scanf("char*",&i);
+    char str[i];
     
-   fscanf(input,"%[^\n]s", str);
+   
+    
+   fscanf(input,"%[^\n]s", str); //fgets(str,stringSize,filePtr);
    printf("we got %s", str);
 
-    
+     
     return 0;
 }
