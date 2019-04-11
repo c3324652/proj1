1429e83255729593fc94d9d2e0c0b8b9b54471f2
matthew ptolemy <c3324652@uon.edu.au>
2019-04-11 05:09:06 +0000

    week 7 lab

--- task1.c
+++ task1.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
+
+    
+
+
+
 int main()
 {
-    123213144525355253
-asdsaddsa
+    FILE *input;
+    input=fopen("data.txt","r");
+        if(input == NULL){
+            perror("fopen()");
+            return 0;
+}
+
+    char str[20];
+    
+   fscanf(input,"%[^\n]s", str);
+   printf("we got %s", str);
+
+    
     return 0;
 }
